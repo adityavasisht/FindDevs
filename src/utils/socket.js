3b5544ed38fb675792d49777d85a71f6dd8c305e
Aditya Vasisht <107064680+adityavasisht@users.noreply.github.com>
2026-02-10 14:17:04 +0530

    Refactor socket initialization and message handing

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,68 +1,74 @@
 const socket = require("socket.io");
-
-const crypto = require("crypto");
 const { Chat } = require("../models/chat");
 
 const initializeSocket = (server) => {
+
   const io = socket(server, {
     cors: {
-
-      origin: ["http://localhost:3002",
-        "http://16.171.12.116",
-        "https://finddevs.xyz"
-       ],
+      origin: [
+        "http://localhost:3002",
+        "http://localhost:5173",
+        "https://finddevs.xyz",
+        "https://www.finddevs.xyz",
+        "http://16.171.12.116"
+      ],
       methods: ["GET", "POST"],
       credentials: true
     },
-  })
+  });
+
+
+
+  io.on("connection", (socket) => {
+    console.log("User Connected: " + socket.id);
+
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+
+      const roomId = [userId, targetUserId].sort().join("_"); 
+      socket.join(roomId);
+      console.log(firstName + " joined " + roomId);
+    });
+
+    socket.on("sendMessage", async ({ firstName, lastName, userId, targetUserId, text }) => {
+
+      const roomId = [userId, targetUserId].sort().join("_");
+
+      try {
+        let chat = await Chat.findOne({
+          participants: { $all: [userId, targetUserId] }
+        });
+
+        if (!chat) {
+          chat = new Chat({
+            participants: [userId, targetUserId],
+            messages: []
+          });
+        }
+
+        chat.messages.push({
+          senderId: userId,
+          text: text, 
+        });
+
+        await chat.save();
+
+        io.to(roomId).emit("messageReceived", {
+          firstName,
+          lastName,
+          text,
+          createdAt: new Date(),
+        });
+
+      } catch (error) {
+        console.log("Error saving chat: " + error);
+      }
+    });
+    
+    socket.on("disconnect", () => {
+        console.log("User Disconnected");
+    });
+  });
 };
 
-io.on("connection", (socket)=>{
-    socket.on("joinChat", ({firstName, userId, targetUserId})=>{
 
-        const roomId = [userId, targetUserId].sort().join(_);
-        socket.join(roomId);
-
-    });
-    socket.on("sendMessage", async({firstName, lastName, userId, targetUserId, message})=>{
-
-        const roomId = [userId, targetUserId].sort().join(_);
-
-
-        try {
-            let chat  = await Chat.findOne({
-                participants: {$all: [userId, targetUserId]}
-
-            });
-            if(!chat){
-                chat = new Chat({
-                    participants: [userId, targetUserId],
-                    messages: []
-                });
-            }
-
-            chat.messages.push({
-                senderId: userId,
-                text: message
-            });
-
-            await chat.save();
-
-            io.to(roomId).emit("messageReceived",{
-                firstName,
-                lastName,
-                message,
-                createdAt: new Date()
-            });
-
-            
-        } catch (error) {
-            console.log("error saving the chat" + error);
-            
-        }
-    } )
-});
-
-  module.exports={
-    initializeSocket
-  }
+module.exports = initializeSocket;
