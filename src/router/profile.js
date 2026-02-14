d708badf5780e66ba8c23c7c384ea6bc50a16bef
Aditya Vasisht <107064680+adityavasisht@users.noreply.github.com>
2026-02-14 19:51:49 +0530

    Update profile.js

--- src/router/profile.js
+++ src/router/profile.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const profileRouter = express.Router();
+const bcrypt = require('bcrypt'); 
 const { authuser } = require("../middlewares/auth.js");
 const User = require('../models/user.js');
 const { validateEditProfileData } = require("../utils/validate.js");
 
-// === ROUTE 1: For Profile/Feed Page ===
+
 profileRouter.get("/profile", authuser, async (req, res) => {
     try {
         const user = req.user;
@@ -14,7 +15,7 @@ profileRouter.get("/profile", authuser, async (req, res) => {
     }
 });
 
-// === ROUTE 2: For Chat Page (Alias) ===
+
 profileRouter.get("/profile/view", authuser, async (req, res) => {
     try {
         const user = req.user;
@@ -24,14 +25,26 @@ profileRouter.get("/profile/view", authuser, async (req, res) => {
     }
 });
 
-// === UPDATE ROUTE ===
+
+
+
 profileRouter.patch("/profile/edit", authuser, async (req, res) => {
     try {
         if (!validateEditProfileData(req)) {
             return res.status(400).send("Invalid Edit Request");
         }
         const user = req.user;
-        Object.keys(req.body).forEach((key) => (user[key] = req.body[key]));
+
+
+        for (const key of Object.keys(req.body)) {
+            if (key === "password") {
+
+                user[key] = await bcrypt.hash(req.body[key], 10);
+            } else {
+                user[key] = req.body[key];
+            }
+        }
+
         await user.save();
         res.json({ message: "Profile updated successfully", data: user });
     } catch (error) {
